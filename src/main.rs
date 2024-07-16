3b234394ed2d4a015a6ef63600c049132ea2604a
DemoDevv <mathieulebras95@gmail.com>
2024-07-16 16:15:29 +0200

    ajout des flags et de la template d'un repository

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use clap::{Arg, Command};
+
+mod templates;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("echo")
+        .version("1.0")
+        .author("DemoDevv")
+        .about("echo est l'outil de production de EF-backend pour générer des fichiers de code")
+        .subcommand(
+            Command::new("create")
+                .about("Crée un nouveau composant")
+                .arg(
+                    Arg::new("composant")
+                        .help("Le type de composant à créer")
+                        .required(true)
+                        .index(1),
+                )
+                .arg(
+                    Arg::new("name")
+                        .help("Le nom du composant à créer")
+                        .required(true)
+                        .index(2),
+                ),
+        )
+        .get_matches();
+
+    if let Some(create_matches) = matches.subcommand_matches("create") {
+        let file_composant = create_matches.get_one::<String>("composant").unwrap();
+        let file_name = create_matches.get_one::<String>("name").unwrap();
+
+        println!("Création d'un {} nommé '{}'", file_composant, file_name);
+
+        // trouver le dossier api au niveau du pwd
+        let api_folder = std::env::current_dir().unwrap().join("api");
+
+        // if !api_folder.exists() {
+        //     println!("Le dossier api n'existe pas");
+        //     return;
+        // }
+
+        match file_composant.as_str() {
+            "repository" => {
+                // un repository et un type vide
+                // ajoute au fichier model.rs la publication du nouveau module et de même pour le repository
+                templates::create_repository(&api_folder, &file_name);
+            }
+            "extractor" => {
+                unimplemented!("Création d'extractor non implémentée")
+            }
+            "handler" => {
+                // créer un handler REST ou alors un handler vide avec uniquement la fonction de configuration des routes vides
+                // a voir si on peut ajouter l'appel à la fonction de configuration des routes dans le fichier routes.rs
+                templates::create_handler(&api_folder, &file_name);
+            }
+            "middleware" => {
+                unimplemented!("Création de middleware non implémentée")
+            }
+            _ => {
+                println!("Type de composant inconnu");
+            }
+        }
+    }
 }
---
+++ src/templates.rs
@@ -0,0 +1,62 @@
+pub(crate) fn create_repository(api_folder: &std::path::Path, file_name: &str) {
+    println!("Création d'un modèle");
+
+    let mut name = file_name.to_lowercase();
+
+    let type_name = name.get_mut(0..1).unwrap().to_uppercase() + name.get_mut(1..).unwrap();
+
+    let name_upper_plural = type_name.clone() + "s";
+
+    // template d'un fichier repository.rs
+    let template_repository_file = r#"use diesel::prelude::*;
+
+use crate::connection::Pool;
+use api_errors::ServiceError;
+use api_types::REPLACE_NAME::NewREPLACE_TYPE;
+
+use crate::repository::{Repository, RepositoryResult};
+
+#[derive(Clone)]
+pub struct REPLACE_NAME_PLURIELRepository {
+    conn: Pool,
+}
+
+impl REPLACE_NAME_PLURIELRepository {
+    pub fn new(conn: Pool) -> Self {
+        Self { conn }
+    }
+}
+
+#[async_trait::async_trait]
+impl Repository<REPLACE_TYPE, NewREPLACE_TYPE> for REPLACE_NAME_PLURIELRepository {
+    async fn get(&self, id: i32) -> RepositoryResult<REPLACE_TYPE> {
+        todo!()
+    }
+
+    async fn get_all(&self) -> RepositoryResult<Vec<REPLACE_TYPE>> {
+        todo!()
+    }
+
+    async fn create(&self, item: &NewREPLACE_TYPE) -> RepositoryResult<REPLACE_TYPE> {
+        todo!()
+    }
+
+    async fn update(&self, id: i32, item: &REPLACE_TYPE) -> RepositoryResult<REPLACE_TYPE> {
+        todo!()
+    }
+
+    async fn delete(&self, id: i32) -> RepositoryResult<usize> {
+        todo!()
+    }
+}
+        "#
+    .replace("REPLACE_NAME_PLURIEL", &name_upper_plural)
+    .replace("REPLACE_TYPE", &type_name)
+    .replace("REPLACE_NAME", &name);
+
+    println!("{}", template_repository_file);
+}
+
+pub(crate) fn create_handler(api_folder: &std::path::Path, file_name: &str) {
+    println!("Création d'un handler");
+}
