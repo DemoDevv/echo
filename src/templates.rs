5c2b77acc22f037a3f4c12c8a069812927fa6d91
DemoDevv <mathieulebras95@gmail.com>
2024-07-22 15:55:56 +0200

    déplacement des fonctions de templates dans un modules différent

--- src/templates.rs
+++ src/templates.rs
@@ -1,3 +1,4 @@
+mod composants;
 mod publish;
 
 use std::{collections::HashMap, path::PathBuf, process::exit};
@@ -6,75 +7,6 @@ use crate::{
     composants::Composant,
     utils::{create_file_with_content, find_api_file, publish_new_module},
 };
-
-pub fn repository(file_name: &str) -> String {
-    let mut name = file_name.to_lowercase();
-    let type_name = name.get_mut(0..1).unwrap().to_uppercase() + name.get_mut(1..).unwrap();
-    let name_upper_plural = type_name.clone() + "s";
-
-    r#"use diesel::prelude::*;
-
-use crate::connection::Pool;
-use crate::models::REPLACE_NAME::REPLACE_TYPE;
-
-use api_errors::ServiceError;
-use api_types::REPLACE_NAME::NewREPLACE_TYPE;
-
-use crate::repository::{Repository, RepositoryResult};
-
-#[derive(Clone)]
-pub struct REPLACE_NAME_PLURIELRepository {
-    conn: Pool,
-}
-
-impl REPLACE_NAME_PLURIELRepository {
-    pub fn new(conn: Pool) -> Self {
-        Self { conn }
-    }
-}
-
-#[async_trait::async_trait]
-impl Repository<REPLACE_TYPE, NewREPLACE_TYPE> for REPLACE_NAME_PLURIELRepository {
-    async fn get(&self, id: i32) -> RepositoryResult<REPLACE_TYPE> {
-        todo!()
-    }
-
-    async fn get_all(&self) -> RepositoryResult<Vec<REPLACE_TYPE>> {
-        todo!()
-    }
-
-    async fn create(&self, item: &NewREPLACE_TYPE) -> RepositoryResult<REPLACE_TYPE> {
-        todo!()
-    }
-
-    async fn update(&self, id: i32, item: &REPLACE_TYPE) -> RepositoryResult<REPLACE_TYPE> {
-        todo!()
-    }
-
-    async fn delete(&self, id: i32) -> RepositoryResult<usize> {
-        todo!()
-    }
-}
-        "#
-    .replace("REPLACE_NAME_PLURIEL", &name_upper_plural)
-    .replace("REPLACE_TYPE", &type_name)
-    .replace("REPLACE_NAME", &name)
-}
-
-fn empty_type(type_name: &str) -> String {
-    r#"#[derive(Serialize, Deserialize)]
-pub struct NewTYPE;"#
-        .replace("TYPE", type_name)
-}
-
-fn empty_model(type_name: &str, pluriel_name: &str) -> String {
-    r#"#[derive(Queryable, Selectable, Serialize, Deserialize)]
-#[diesel(table_name = PLURIEL_NAME)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
-pub struct TYPE;"#
-        .replace("TYPE", type_name)
-        .replace("PLURIEL_NAME", pluriel_name)
-}
 
 pub(crate) fn create_repository(api_folder: &std::path::Path, file: Composant) {
     let file_name = if let Composant::Repository(name) = file {
@@ -139,11 +71,14 @@ pub(crate) fn create_repository(api_folder: &std::path::Path, file: Composant) {
     let mod_type_path = find_api_file(api_folder, Composant::Type, "lib.rs")
         .expect("Le fichier lib.rs est introuvable");
 
-    create_file_with_content(&repository_path_buf, &repository(&file_name))
+    create_file_with_content(&repository_path_buf, &composants::repository(&file_name))
         .expect("Erreur lors de la création du nouveau repository");
-    create_file_with_content(&model_path_buf, &empty_model(&type_name, &name_plural))
-        .expect("Erreur lors de la création du nouveau model");
-    create_file_with_content(&type_path_buf, &empty_type(&type_name))
+    create_file_with_content(
+        &model_path_buf,
+        &composants::empty_model(&type_name, &name_plural),
+    )
+    .expect("Erreur lors de la création du nouveau model");
+    create_file_with_content(&type_path_buf, &composants::empty_type(&type_name))
         .expect("Erreur lors de la création du nouveau type");
 
     publish_new_module(&mod_repository_path, &publish::mod_repository(&name_plural))
---
+++ src/templates/composants.rs
@@ -0,0 +1,68 @@
+pub(crate) fn repository(file_name: &str) -> String {
+    let mut name = file_name.to_lowercase();
+    let type_name = name.get_mut(0..1).unwrap().to_uppercase() + name.get_mut(1..).unwrap();
+    let name_upper_plural = type_name.clone() + "s";
+
+    r#"use diesel::prelude::*;
+
+use crate::connection::Pool;
+use crate::models::REPLACE_NAME::REPLACE_TYPE;
+
+use api_errors::ServiceError;
+use api_types::REPLACE_NAME::NewREPLACE_TYPE;
+
+use crate::repository::{Repository, RepositoryResult};
+
+#[derive(Clone)]
+pub struct REPLACE_NAME_PLURIELRepository {
+    conn: Pool,
+}
+
+impl REPLACE_NAME_PLURIELRepository {
+    pub fn new(conn: Pool) -> Self {
+        Self { conn }
+    }
+}
+
+#[async_trait::async_trait]
+impl Repository<REPLACE_TYPE, NewREPLACE_TYPE> for REPLACE_NAME_PLURIELRepository {
+    async fn get(&self, id: i32) -> RepositoryResult<REPLACE_TYPE> {
+        todo!()
+    }
+
+    async fn get_all(&self) -> RepositoryResult<Vec<REPLACE_TYPE>> {
+        todo!()
+    }
+
+    async fn create(&self, item: &NewREPLACE_TYPE) -> RepositoryResult<REPLACE_TYPE> {
+        todo!()
+    }
+
+    async fn update(&self, id: i32, item: &REPLACE_TYPE) -> RepositoryResult<REPLACE_TYPE> {
+        todo!()
+    }
+
+    async fn delete(&self, id: i32) -> RepositoryResult<usize> {
+        todo!()
+    }
+}
+        "#
+    .replace("REPLACE_NAME_PLURIEL", &name_upper_plural)
+    .replace("REPLACE_TYPE", &type_name)
+    .replace("REPLACE_NAME", &name)
+}
+
+pub(crate) fn empty_type(type_name: &str) -> String {
+    r#"#[derive(Serialize, Deserialize)]
+pub struct NewTYPE;"#
+        .replace("TYPE", type_name)
+}
+
+pub(crate) fn empty_model(type_name: &str, pluriel_name: &str) -> String {
+    r#"#[derive(Queryable, Selectable, Serialize, Deserialize)]
+#[diesel(table_name = PLURIEL_NAME)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct TYPE;"#
+        .replace("TYPE", type_name)
+        .replace("PLURIEL_NAME", pluriel_name)
+}
